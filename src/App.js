9c0a5794bf61a9198910fdd72982fce2c6395d66
Makoto Ishibashi <ishibashi.makoto@jp.panasonic.com>
2021-01-21 15:26:41 +0900

    変数の使用

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, {Component} from 'react';
+// import {Component} from 'react';
 
+
+// // クラスの場合
+// class App extends Component{
+//   render(){
+//     return <h1>Hello world!</h1>;
+//   }
+// }
+
+// 関数コンポーネントの場合
 function App() {
-  return <h1 className="App">Hello World!!</h1>;
+  const child = "Basio";
+  const dom = <h1 className="App"/* classNameはHTML上のclassに変更される */>Hi, {child}</h1> // {}を使うことで変数を使える
+  return dom;
 }
 
-// 下記のようにコンパイルされる
+// 下記のようにトランスファイルされる
 // function App(){
 //   return React.createElement('div', null, 'Hello W orld!!!');
 // }

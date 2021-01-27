574e2119b7f6667fb61f55d08e6da0f50dccfc43
Makoto Ishibashi <ishibashi.makoto@jp.panasonic.com>
2021-01-27 14:41:50 +0900

    implement event new component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 // アプリケーション内のReducerを結合するモジュール
 import {combineReducers} from 'redux'
+import {reducer as form} from 'redux-form'
 import events from './events'
 
-export default combineReducers({events}) // storeで利用される
+export default combineReducers({events, form}) // storeで利用される
 // export default combineReducers({foo, bar, buz}) // 複数状態を管理したい場合はこのように書く
